65270214cd6fa729e40858ba08c54082a8e50ba9
Gustavo Ferreira <gustavojcoferreira@gmail.com>
2019-11-23 01:37:52 +0000

    Initial work on the cache

---
+++ pkg/exchangestream/cache/market.go
@@ -0,0 +1,44 @@
+package cache
+
+type MarketCache struct {
+	Clk          string
+	InitialClk   string
+	PublishTime  *int
+	MarketId     string
+	TradedVolume *float64
+	// MarketDefinition *MarketDefinition
+	Runners map[int64]RunnerCache // key is the RunnerID
+}
+
+func NewMarketCache() MarketCache {
+	mc := MarketCache{}
+
+	return mc
+}
+
+func (mc *MarketCache) Update() {
+
+}
+
+type RunnerCache struct {
+	SelectionId                int64
+	LastTradedPrice            *float64
+	TradedVolume               *float64
+	StartingPriceNear          *float64
+	StartingPriceFar           *float64
+	Traded                     []PriceStep
+	AvailableToBack            []PriceStep
+	AvailableToLay             []PriceStep
+	StartingPriceBack          []PriceStep
+	StartingPriceLay           []PriceStep
+	BestAvailableToBack        []PriceStep
+	BestAvailableToLay         []PriceStep
+	BestDisplayAvailableToBack []PriceStep
+	BestDisplayAvailableToLay  []PriceStep
+}
+
+type PriceStep struct {
+	Position float64
+	Price    float64
+	Size     float64
+}
---
+++ pkg/exchangestream/cache/order.go
@@ -0,0 +1,10 @@
+package cache
+
+type OrderCache struct {
+}
+
+func NewOrderCache() OrderCache {
+	oc := OrderCache{}
+
+	return oc
+}
